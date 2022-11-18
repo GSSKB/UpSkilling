d7d8e7dece96ab0f29239cff682293b3a28771ba
Akhil <sai26091999@gmail.com>
2022-11-18 14:43:55 +0530

    Initial Commit

---
+++ FileParsing/Students Data/Student data.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Student data.aspx.cs" Inherits="Students_Data.Student_data" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:ListBox ID="ListBox1" runat="server" Height="315px" Width="315px"></asp:ListBox>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Button" Width="170px" />
+            <asp:Button ID="Button2" runat="server" Text="Button" Width="143px" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ FileParsing/Students Data/Student data.aspx.cs
@@ -0,0 +1,61 @@
+﻿/*using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Students_Data
+{
+    public partial class Student_data : System.Web.UI.Page
+    {
+       
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                ArrayList studentdata = new ArrayList();
+                try
+                {
+                    int index = 0;
+                    Char[] sep = { ',' };
+
+                    string[] lines = File.ReadAllLines("D:\\.NET\\Student1\\Customer\\bin\\Debug\\sai.txt");
+                    foreach (string line in lines)
+
+                    {
+
+                        if (index == 0)
+                        {
+                            index++;
+                            continue;
+                        }
+                        string[] data = line.Split(sep);
+
+                        studentdata.Add(data);
+                    }
+
+                }
+                catch (FileNotFoundException ex)
+                {
+                    Console.WriteLine("The file was not found on your selected path");
+                    Console.WriteLine(ex.Message);
+                }
+                foreach (string[] student in studentdata)
+                {
+                    //foreach(string stu in student)
+                    for (int i = 0; i < student.Length; i++)
+                    {
+                        //Console.WriteLine(student[i].ToString());
+                        ListBox1.Items.Add(student[i]);
+                    }
+
+                }
+            }
+
+        }
+    }
+}*/
